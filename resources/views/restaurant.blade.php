035f1484fe37fb0909c4a5bf5c60d84e327a03e6
Jonas Pauli <jonaspauli@me.com>
2022-10-05 16:26:31 +0200

    init

---
+++ resources/views/restaurant.blade.php
@@ -0,0 +1,13 @@
+{{--
+  Template Name: Restaurant
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php(the_post())
+    @include('partials.page-header')
+    @include('partials.content-page')
+    <h1>Einfach Ankommen</h1>
+  @endwhile
+@endsection
