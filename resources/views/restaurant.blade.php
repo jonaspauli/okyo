42d82fe85a31b8b4220b87b01c1d1a010b032408
Jonas Pauli <jonaspauli@me.com>
2022-10-05 22:10:12 +0200

    Adding Fonts und Start Restaurant

--- resources/views/restaurant.blade.php
+++ resources/views/restaurant.blade.php
@@ -8,6 +8,9 @@
   @while(have_posts()) @php(the_post())
     @include('partials.page-header')
     @include('partials.content-page')
-    <h1>Einfach Ankommen</h1>
+    <div class="container grid place-content-center mx-auto h-96 bg-okyo-black text-okyo-white font-sans font-normal bg-logo bg-no-repeat bg-center bg-[length:340px]">
+      <h1 class="text-center">Einfach Ankommen</h1>
+    </div>
+
   @endwhile
 @endsection
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,19 @@
 module.exports = {
   content: ["./index.php", "./app/**/*.php", "./resources/**/*.{php,vue,js}"],
   theme: {
-    extend: {
-      colors: {},
+    colors: {
+      'okyo-orange' : '#FFB800',
+      'okyo-black' : '#0F0B00',
+      'okyo-white' : '#F3F1EC',
     },
-  },
+    fontFamily: {
+      sans: ['Spartan', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        'logo': "url('~@images/logo_hero.svg')",
+      }
+    },
   plugins: [],
-};
+}
+}
