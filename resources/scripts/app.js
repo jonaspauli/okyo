358642477177808443cd244801c4a3859d9337b5
Jonas Pauli <jonaspauli@me.com>
2022-12-23 18:03:37 +0100

    V1.1

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -142,6 +142,97 @@ $('.gallery-rellax').each(function() {
       });
      });
 
+
+/// Text Scroll
+
+var textband = $('.textband');
+
+// Support small text - copy to fill screen width
+if (textband.find('.scrolling-text').outerWidth() < $(window).width()) {
+    var windowToScrolltextRatio = Math.round($(window).width() / textband.find('.scrolling-text').outerWidth()),
+        scrollTextContent = textband.find('.scrolling-text .scrolling-text-content').text(),
+        newScrollText = '';
+    for (var i = 0; i < windowToScrolltextRatio; i++) {
+        newScrollText += ' ' + scrollTextContent;
+    }
+    textband.find('.scrolling-text .scrolling-text-content').text(newScrollText);
+}
+
+// Init variables and config
+var scrollingText = textband.find('.scrolling-text'),
+    scrollingTextWidth = scrollingText.outerWidth(),
+    scrollingTextHeight = scrollingText.outerHeight(true),
+    startLetterIndent = parseInt(scrollingText.find('.scrolling-text-content').css('font-size'), 10) / 4.8,
+    startLetterIndent = Math.round(startLetterIndent),
+    scrollAmountBoundary = Math.abs($(window).width() - scrollingTextWidth),
+    transformAmount = 0,
+    leftBound = 0,
+    rightBound = scrollAmountBoundary,
+    transformDirection = textband.hasClass('left-to-right') ? -1 : 1,
+    transformSpeed = 200;
+
+// Read transform speed
+if (textband.attr('speed')) {
+    transformSpeed = textband.attr('speed');
+}
+
+// Make scrolling text copy for scrolling infinity
+textband.append(scrollingText.clone().addClass('scrolling-text-copy'));
+textband.find('.scrolling-text').css({'position': 'absolute', 'left': 0});
+textband.css('height', scrollingTextHeight);
+
+var getActiveScrollingText = function(direction) {
+    var firstScrollingText = textband.find('.scrolling-text:nth-child(1)');
+    var secondScrollingText = textband.find('.scrolling-text:nth-child(2)');
+
+    var firstScrollingTextLeft = parseInt(textband.find('.scrolling-text:nth-child(1)').css("left"), 10);
+    var secondScrollingTextLeft = parseInt(textband.find('.scrolling-text:nth-child(2)').css("left"), 10);
+
+    if (direction === 'left') {
+        return firstScrollingTextLeft < secondScrollingTextLeft ? secondScrollingText : firstScrollingText;
+    } else if (direction === 'right') {
+        return firstScrollingTextLeft > secondScrollingTextLeft ? secondScrollingText : firstScrollingText;
+    }
+}
+
+$(window).on('wheel', function(e) {
+    var delta = e.originalEvent.deltaY;
+  console.log(delta);
+
+    // Skew on scroll
+    if (delta > 0) {
+        // going down
+        transformAmount += transformSpeed * transformDirection;
+        textband.find('.scrolling-text .scrolling-text-content').css('transform', 'skewX(10deg)');
+    }
+    else {
+        transformAmount -= transformSpeed * transformDirection;
+        textband.find('.scrolling-text .scrolling-text-content').css('transform', 'skewX(-10deg)');
+    }
+
+    
+    setTimeout(function(){
+        textband.find('.scrolling-text').css('transform', 'translate3d('+ transformAmount * -1 +'px, 0, 0)');
+    }, 10);
+    setTimeout(function() {
+        textband.find('.scrolling-text .scrolling-text-content').css('transform', 'skewX(0)');
+    }, 500)
+
+    // Boundaries
+    if (transformAmount < leftBound) {
+        var activeText = getActiveScrollingText('left');
+        activeText.css({'left': Math.round(leftBound - scrollingTextWidth - startLetterIndent) + 'px'});
+        leftBound = parseInt(activeText.css("left"), 10);
+        rightBound = leftBound + scrollingTextWidth + scrollAmountBoundary + startLetterIndent;
+
+    } else if (transformAmount > rightBound) {
+        var activeText = getActiveScrollingText('right');
+        activeText.css({'left': Math.round(rightBound + scrollingTextWidth - scrollAmountBoundary + startLetterIndent) + 'px'});
+        rightBound += scrollingTextWidth + startLetterIndent;
+        leftBound = rightBound - scrollingTextWidth - scrollAmountBoundary - startLetterIndent;
+    }
+});
+
 /// Startseite
 
 
