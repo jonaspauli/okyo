afdcfb7a5747f76d40e987dbf46aa7c8a5aadf9a
Jonas Pauli <jonaspauli@me.com>
2022-12-23 19:20:01 +0100

    Different Gallery

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -9,19 +9,32 @@
     @include('partials.page-header')
     @include('partials.content-page')
   @endwhile
-  <div class="gallery-container">
-  <?php if( have_rows('gallery') ): ?>
-    <div class="gallery">
-    <?php while( have_rows('gallery') ): the_row(); ?>
-    <div class="gallery-image h-screen w-screen overflow-hidden">
-        <?php $image = get_sub_field('image');#
+
+<section class="container <?php echo $class_name ?>">
+  <?php if( have_rows('gallery') ):
+      $row = get_field('gallery');
+      $i = 1;
+    ?>
+    <div class="flex flex-row flex-wrap mb-[15rem]">
+        <?php while( have_rows('gallery') ): the_row();?>
+        <?php
+            if ($i % 2 != 0) {
+            echo "<div class=\"w-full sm:w-1/2 sm:mr-[-2rem] sm:mb-[-10rem]\">";
+            } else {
+            echo "<div class=\"w-full sm:w-1/2 sm:mt-[15rem] sm:ml-[-2rem] sm:mb-[-10rem]\">";
+            }
+        ?>
+            <div class="z-<?php echo $i ?>0 my-lg-4 mb-4 mb-lg-0 relative projekte-fade gallery-rellax">
+            <?php $image = get_sub_field('image');#
         if( !empty( $image ) ): ?>
           <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="w-full" />
       <?php endif; ?>
             </div>
-    <?php endwhile; ?>
-    
+        </div>
+            <?php $i++; ?>
+        <?php  endwhile;?>
     </div>
-<?php endif; ?>
-</div>
+
+  <?php endif; ?>
+</section>
 @endsection
